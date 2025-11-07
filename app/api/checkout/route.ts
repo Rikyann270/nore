44098ab4cb9e9d9c81977598a3509116c7824b15
Rikyann270 <125094333+Rikyann270@users.noreply.github.com>
2025-11-07 19:13:52 +0300

    payment fix

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -53,11 +53,11 @@ export async function POST(request: NextRequest) {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/checkout`,
+      success_url: `${"https://noreskincare.vercel.app/" || "http://localhost:3000"}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${"https://noreskincare.vercel.app/" || "http://localhost:3000"}/checkout`,
       customer_email: customer.email,
       shipping_address_collection: {
-        allowed_countries: ["US", "CA", "GB"],
+        allowed_countries: ["US", "CA", "GB", "UG"],
       },
       metadata: {
         firstName: customer.firstName,
